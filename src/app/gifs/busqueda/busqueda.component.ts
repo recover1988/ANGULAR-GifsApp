f2d9fb09af37d9d354888a947f721efe6f769f1a
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-10 16:13:48 -0300

    create service and use ngFor

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -9,11 +10,15 @@ export class BusquedaComponent {
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
+  constructor(private gifsService: GifsService) { }
+
   buscar() {
 
     // document.querySelector('input').value = '';
     const valor = this.txtBuscar.nativeElement.value;
-    console.log(valor)
+
+    this.gifsService.buscarGifs(valor)
+
     this.txtBuscar.nativeElement.value = '';
   }
 
