42cc69d1687d36aad93dbbc55b6bbdbaf13a47f1
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-09 23:05:17 -0300

    ViewChild funcion como el document.selectquery

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
 })
+// Not null assertion operator -->!
 export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  buscar() {
+
+    // document.querySelector('input').value = '';
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log(valor)
+    this.txtBuscar.nativeElement.value = '';
+  }
 
 }
