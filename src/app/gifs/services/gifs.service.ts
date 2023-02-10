a9ba476d984278f92712b731bef0e17c155314bf
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-10 18:12:52 -0300

    add httpclientmodule and httpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root' // se eleva a un servicio global
 })
+
 export class GifsService {
   private apiKey: string = 'dWbtzvoxXfl6EOBk4Nsnc74t7AQXzrcO';
   private _historial: string[] = [];
@@ -11,6 +13,9 @@ export class GifsService {
     // this._historial = this._historial.splice(0, 10)
     return [...this._historial];
   }
+
+  // injectamos el HttpClient que es un observable
+  constructor(private http: HttpClient) { }
 
   buscarGifs(query: string) {
 
@@ -21,9 +26,21 @@ export class GifsService {
       this._historial = this._historial.splice(0, 10)
 
     }
+    this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=dWbtzvoxXfl6EOBk4Nsnc74t7AQXzrcO&q=${query}&limit=10`)
+      .subscribe((resp: any) => {
+        console.log(resp.data)
+      })
 
 
-    console.log(this._historial)
   }
-  constructor() { }
+
 }
+
+    // fetch('http://api.giphy.com/v1/gifs/search?api_key=dWbtzvoxXfl6EOBk4Nsnc74t7AQXzrcO&q=dragon ball z&limit=10')
+    //   .then(resp => {
+    //     resp.json().then(data => console.log(data))
+    //   })
+
+    // const resp = await fetch('http://api.giphy.com/v1/gifs/search?api_key=dWbtzvoxXfl6EOBk4Nsnc74t7AQXzrcO&q=dragon ball z&limit=10')
+    // const data = await resp.json();
+    // console.log(data)
